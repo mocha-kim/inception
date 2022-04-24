14d944946ec8dbdf067600d857667be42355f534
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-04-24 17:33:49 +0900

    fix

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -3,15 +3,13 @@ chown -R mysql:mysql /var/lib/mysql
 
 # init
 service mysql start
-mysql
+echo "flush privileges;" | mysql -u root
 
-echo "flush privileges;"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_ROOT_PWD';" | mysql -u root
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_ROOT_PWD';"
-
-echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
-echo "CREATE USER IF NOT EXISTS '$DATABASE_USR'@'%' IDENTIFIED BY '$DATABASE_PWD';"
-echo "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_USR'@'%';"
+echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;" | mysql -u root
+echo "CREATE USER IF NOT EXISTS '$DATABASE_USR'@'%' IDENTIFIED BY '$DATABASE_PWD';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_USR'@'%';" | mysql -u root
 
 echo "flush privileges;"
 service mysql stop
