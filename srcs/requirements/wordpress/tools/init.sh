0dbbd09f6d74358b29f91e83dde411dd4c7c3e98
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-04-24 18:24:02 +0900

    fix

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -8,8 +8,8 @@ mv /tmp/wp-config.php /var/www/html/
 chown -R www-data:www-data /var/www/html/
 
 sed -i -e "s|DATABASE_NAME|'$DATABASE_NAME'|g" /var/www/html/wp-config.php
-sed -i -e "s|DATABASE_USR|'$MYSQL_USER'|g" /var/www/html/wp-config.php
-sed -i -e "s|DATABASE_PWD|'$MYSQL_PASSWORD'|g" /var/www/html/wp-config.php
+sed -i -e "s|DATABASE_USR|'$DATABASE_USR'|g" /var/www/html/wp-config.php
+sed -i -e "s|DATABASE_PWD|'$DATABASE_PWD'|g" /var/www/html/wp-config.php
 sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
 sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
 sed -i -e "s|;clear_env = no|clear_env = no|g" /etc/php/7.3/fpm/pool.d/www.conf
