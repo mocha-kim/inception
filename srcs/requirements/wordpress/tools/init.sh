4dde56972dae0a850bbce3e270110b5c625fe76d
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-04-24 18:06:52 +0900

    fix

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,13 +1,14 @@
 #!bin/bash/
-mv wordpress/ /var/www/wordpress/
+mv wordpress/ /var/www/html/
+cat "wordpress files moved"
 rm latest.tar.gz
 
 mv /tmp/wp-config.php /var/www/wordpress/
 chown -R www-data:www-data /var/www/wordpress/
 
-sed -i -e "s|DATABASE_NAME|'$DATABASE_NAME'|g" /var/www/wordpress/wp-config.php
-sed -i -e "s|MYSQL_USER|'$MYSQL_USER'|g" /var/www/wordpress/wp-config.php
-sed -i -e "s|MYSQL_PASSWORD|'$MYSQL_PASSWORD'|g" /var/www/wordpress/wp-config.php
+sed -i -e "s|DATABASE_NAME|'$DATABASE_NAME'|g" /var/www/html/wp-config.php
+sed -i -e "s|MYSQL_USER|'$MYSQL_USER'|g" /var/www/html/wp-config.php
+sed -i -e "s|MYSQL_PASSWORD|'$MYSQL_PASSWORD'|g" /var/www/html/wp-config.php
 sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
 sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
 sed -i -e "s|;clear_env = no|clear_env = no|g" /etc/php/7.3/fpm/pool.d/www.conf
