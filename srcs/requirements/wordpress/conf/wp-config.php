cbc025a38310736af3c019011f8672c714212e9b
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-04-26 18:38:32 +0900

    wordpress

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', DATABASE_NAME );
+define( 'DB_NAME', getenv('DATABASE_NAME') );
 
 /** MySQL database username */
-define( 'DB_USER', DATABASE_USR );
+define( 'DB_USER', getenv('DATABASE_USR') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', DATABASE_PWD );
+define( 'DB_PASSWORD', getenv('DATABASE_PWD') );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mariadb:3306' );
