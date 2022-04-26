7b3a2ac60df1be8afb69e02f7abbd445f307e42b
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-04-26 15:23:45 +0900

    host name

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -1,6 +1,3 @@
-echo "127.0.0.1	$DOMAIN_NAME" >> /etc/hosts
-echo "$DOMAIN_NAME" >> /etc/hostname
-echo "nameserver	127.0.0.1" >> /etc/resolv.conf
 sed -i -e "s|DOMAIN_NAME|$DOMAIN_NAME|g" /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
