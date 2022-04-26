d1d31a3109152c921911f5299bb22ffb6de36681
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-04-26 14:43:19 +0900

    domain

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -1,4 +1,5 @@
 echo "127.0.0.1	$DOMAIN_NAME" >> /etc/hosts
+echo "127.0.0.1:443	$DOMAIN_NAME" >> /etc/hosts
 sed -i -e "s|DOMAIN_NAME|$DOMAIN_NAME|g" /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
