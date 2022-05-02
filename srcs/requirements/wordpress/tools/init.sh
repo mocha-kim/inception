885719b8b207a830ad1fc09b1afef43b59465bb0
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-05-02 17:56:25 +0900

    wp user

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,9 +1,9 @@
 #!bin/bash/
-chown -R www-data:www-data /var/www/wordpress/
 mv /tmp/wp-config.php /var/www/wordpress/
 mv /tmp/www.conf /etc/php/7.3/fpm/pool.d/www.conf
 mv /wordpress/* /var/www/wordpress/
 rm -rf /wordpress
+chown -R www-data:www-data /var/www/wordpress/
 
 wp core download --version=5.8.1 --path=/var/www/wordpress --allow-root
 sed -i -e "s|DATABASE_NAME|'$DATABASE_NAME'|g" /var/www/wordpress/wp-config.php
@@ -12,9 +12,9 @@ sed -i -e "s|DATABASE_PWD|'$DATABASE_PWD'|g" /var/www/wordpress/wp-config.php
 
 sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
 
-wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --path=/var/www/wordpress/
-wp plugin update --all
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD
-wp post create --post_title="inception" --post_content="content" --post_status=publish --post_author="unknown"
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --path=/var/www/wordpress/ --allow-root
+wp plugin update --all --allow-root
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+sudo -u sunhkim -i -- wp post create --post_title="inception" --post_content="content" --post_status=publish --post_author="unknown"
 
 exec /usr/sbin/php-fpm7.3
