4ae3512f92c9c68b51525f512505161e3a75d779
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-05-05 16:37:11 +0900

    mariadb

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 chown -R mysql:mysql /var/lib/mysql
 
+sed -ie 's/bind-address/#bind-address/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -ie 's/#port/port/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
 # init
 service mysql start
 
@@ -11,7 +14,7 @@ DELETE FROM mysql.user WHERE User='$DATABASE_ROOT' AND Host NOT IN ('localhost',
 FLUSH PRIVILEGES;
 EOF
 
-mysql --user=$DATABASE_ROOT --password=$DATABASE_ROOT_PWD <<EOF
+mysql --user=$DATABASE_ROOT --password=$DATABASE_ROOT_PWD << EOF
 CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;
 CREATE USER IF NOT EXISTS '$DATABASE_USR'@'%' IDENTIFIED BY '$DATABASE_PWD';
 GRANT ALL PRIVILEGES ON *.* TO '$DATABASE_ROOT'@'%' IDENTIFIED BY '$DATABASE_ROOT_PWD';
