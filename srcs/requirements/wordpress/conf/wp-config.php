91a85ec03901136cfd6364e5628ed058ffa23339
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-05-05 16:08:42 +0900

    wordpress

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', DATABASE_NAME );
+define( 'DB_NAME', getenv('DATABASE_NAME') );
 
 /** MySQL database username */
-define( 'DB_USER', DATABASE_USR );
+define( 'DB_USER', getenv('DATABASE_USR') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', DATABASE_PWD );
+define( 'DB_PASSWORD', getenv('DATABASE_PWD') );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mariadb:3306' );
+define( 'DB_HOST', getenv('DATABASE_HOST') );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -4,10 +4,6 @@ mv /tmp/wp-config.php /var/www/wordpress/
 mv /tmp/www.conf /etc/php/7.3/fpm/pool.d/www.conf
 chown -R www-data:www-data /var/www/wordpress/
 
-sed -i -e "s|DATABASE_NAME|'$DATABASE_NAME'|g" /var/www/wordpress/wp-config.php
-sed -i -e "s|DATABASE_USR|'$DATABASE_USR'|g" /var/www/wordpress/wp-config.php
-sed -i -e "s|DATABASE_PWD|'$DATABASE_PWD'|g" /var/www/wordpress/wp-config.php
-
 sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
 
 wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --path=/var/www/wordpress --allow-root
