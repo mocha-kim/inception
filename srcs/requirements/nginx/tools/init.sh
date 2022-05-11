541eebecf5cd6d58c1b6eefb9792ff42282fadda
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-05-11 13:35:17 +0900

    conf

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -1,4 +1,5 @@
 sed -ie "s|DOMAIN_NAME|$DOMAIN_NAME|g" /tmp/default.conf
-cp -f /tmp/default.conf /etc/nginx/sites-available/default
+cp -f /tmp/default.conf /etc/nginx/nginx.conf
+rm -f /etc/nginx/sites-enabled/default
 
 nginx -g 'daemon off;'
