0e0e72e41dd6ceee7f831f28abf3ebf7a594f420
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-05-11 20:37:23 +0900

    wordpress conf

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -9,7 +9,7 @@ chown -R www-data:www-data /var/www/wordpress/
 usermod -a -G www-data www-data
 sed -ie "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
 
-sleep 100;
+sleep 10;
 wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --path=/var/www/wordpress --allow-root
 wp plugin update --all --path=/var/www/wordpress --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --path=/var/www/wordpress --allow-root
